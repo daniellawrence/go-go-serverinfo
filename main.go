9a680f8c4256f55b8009965dc9508354348a4809
Daniel Lawrence <dannyla@linux.com>
2015-09-09 21:29:51 -0700

    mem + swap info

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/shirou/gopsutil/host"
 	"github.com/shirou/gopsutil/cpu"
 	"github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/mem"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +19,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func json_hostinfo(w http.ResponseWriter, r *http.Request) {
 	hostinfo, _ := host.HostInfo()
 	b, _ := json.Marshal(hostinfo)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, string(b))
+}
+
+func json_swapinfo(w http.ResponseWriter, r *http.Request) {
+	swapinfo, _ := mem.SwapMemory()
+	b, _ := json.Marshal(swapinfo)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, string(b))
+}
+
+func json_meminfo(w http.ResponseWriter, r *http.Request) {
+	memginfo, _ := mem.VirtualMemory()
+	b, _ := json.Marshal(memginfo)
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprintf(w, string(b))
 }
@@ -44,14 +59,14 @@ func json_diskIOCounters(w http.ResponseWriter, r *http.Request) {
 
 func embed(w http.ResponseWriter, r *http.Request) {
 	data, _ := Asset("static/index.html")
-	fmt.Println("static!")
+	fmt.Println("embed!")
 	fmt.Fprintf(w, string(data))
 }
 
 func main() {
-	static_dir := http.FileServer(http.Dir("static"))
+	static_dir := http.FileServer(http.Dir("./static"))
 
-	portPtr := flag.Int("port", 3000, "Port to bind go-go-server-info")
+	portPtr := flag.Int("port", 3001, "Port to bind go-go-server-info")
 	bindPtr := flag.String("host", "0.0.0.0", "bind IP")
 
 	flag.Parse()
@@ -59,12 +74,15 @@ func main() {
 
 
 	log.Println("Listening... " + bind)	
+	http.Handle("/dev", static_dir)
+	http.Handle("/dev/", static_dir)
 	http.HandleFunc("/", embed)
 	http.HandleFunc("/json/hostinfo/", json_hostinfo)
 	http.HandleFunc("/json/cpuinfo/", json_cpuinfo)
 	http.HandleFunc("/json/diskpartitions/", json_diskpartitions)
 	http.HandleFunc("/json/diskiocounters/", json_diskIOCounters)
-	http.Handle("/dev", static_dir)
+	http.HandleFunc("/json/meminfo/", json_meminfo)
+	http.HandleFunc("/json/swapinfo/", json_swapinfo)
 	err := http.ListenAndServe(bind, nil)
 
 	if err != nil {
